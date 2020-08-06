77fc453a02c33ce261f4659a4a445070a071a8d5
Crow <e-amce@gft.com>
2020-08-06 16:52:16 +0100

    assign objectadmin role for sink writer to bucket

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -135,13 +135,25 @@ module "audit-log-sink-creation" {
   log_sink_filter      = "logName=(folders/${var.root_id}/logs/cloudaudit.googleapis.com%2Factivity OR folders/${var.root_id}/logs/cloudaudit.googleapis.com%2Fdata_access)"
 }
 
-module "audit-log-writer-binding" {
-  source = "../../project-iam-binding-creator"
+#module "audit-log-writer-binding" {
+#  source = "../../project-iam-binding-creator"
+#
+#  project = module.shared_projects.shared_telemetry_id
+#  members = module.audit-log-sink-creation.log_sink_writer
+#  role    = var.audit_iam_role
+#}
 
-  project = module.shared_projects.shared_telemetry_id
-  members = module.audit-log-sink-creation.log_sink_writer
-  role    = var.audit_iam_role
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = "storage.googleapis.com/${module.audit-log-bucket.name}"
+  role = "roles/storage.objectAdmin"
+  members = [
+    module.audit-log-sink-creation.log_sink_writer,
+  ]
 }
+
+
+
+
 
 #####
 
