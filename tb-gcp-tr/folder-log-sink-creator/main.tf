2548dd3318196a11f1189a9e9706aa7f555961ba
Crow <e-amce@gft.com>
2020-08-10 10:52:48 +0100

    removed folder log sink creator folder

--- tb-gcp-tr/folder-log-sink-creator/main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_logging_folder_sink" "folder_log_sink" {
-  folder           = var.log_sink_folder
-  name             = var.log_sink_name
-  destination      = var.log_sink_destination
-  filter           = var.log_sink_filter
-  include_children = var.include_children
-}
--- tb-gcp-tr/folder-log-sink-creator/output.tf
+++
@@ -1,3 +0,0 @@
-output "log_sink_writer" {
-  value = google_logging_folder_sink.folder_log_sink.writer_identity
-}
