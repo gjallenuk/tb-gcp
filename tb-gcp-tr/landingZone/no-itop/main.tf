f1f3e72d7f3422de483c2ff5fcfc2ffaa5b969bc
Crow <e-amce@gft.com>
2020-08-21 11:05:42 +0100

    pinned bucket version to 1.6. for audit-log-bucket

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -116,7 +116,7 @@ module "shared-vpc" {
 ##### Audit logging #####
 
 module "audit-log-bucket" {
-  source = "github.com/tranquilitybase-io/terraform-google-cloud-storage.git//modules/simple_bucket?ref=logging"
+  source = "github.com/tranquilitybase-io/terraform-google-cloud-storage.git//modules/simple_bucket?ref=v1.6.0-logging"
 
   project_id      = module.shared_projects.shared_telemetry_id
   name            = "${var.audit_bucket_name}-${var.tb_discriminator}"
